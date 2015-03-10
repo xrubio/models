98bdbb93a015b9d4cfadeb908bdcc5ba6a6bae51
Enrico Crema <enrico.crema@gmail.com>
2015-03-10 14:52:14 +0100

    Updated Decision making code

--- modelling_sss/src/R/decisionMaking.R
+++ modelling_sss/src/R/decisionMaking.R
@@ -5,44 +5,41 @@
 # Parameters:                                       
 # decisionType ::: Type of decision, either "greedy" or "probabilistic"
 # memory ::: Boolean, whether the agents move based on current state of the system, or their cognitive maps
-# xDim ::: x limit of the world
-# yDim ::: y limit of the world
+# dimX ::: x limit of the world
+# dimY ::: y limit of the world
 # nAgents ::: Number of agents at initialisation                                        
-# timeSteps ::: Number of timesteps in the model
+# nSteps ::: Number of timesteps in the model
 # resourceGrowthRate ::: Amount increase of resources per timeStep
 # maxEnergy ::: maximum possible storable energy. Also maximum possible cell values in the resource scape. 
-# spendEnergy ::: ammount of enegry spent by each agent each timestep.
+# energyCost ::: ammount of enegry spent by each agent each timestep.
 
 
-nAgents=100;spendEnergy=9;maxEnergy=100;resourceGrowthRate=2;
-               timeSteps=1000;xDim=30;yDim=30;memory=FALSE;
-               decisionType=c("greedy","probabilistic");
-plot=TRUE;verbose=TRUE
+nAgents=100;energyCost=9;maxEnergy=100;resourceGrowthRate=2;nSteps=1000;dimX=30;dimY=30;memory=FALSE;decisionType=c("greedy");plot=TRUE;verbose=TRUE
 
 
 
-main<-function(nAgents=100,spendEnergy=9,maxEnergy=100,resourceGrowthRate=2,
-               timeSteps=1000,xDim=30,yDim=30,memory=FALSE,
+main<-function(nAgents=100,energyCost=9,maxEnergy=100,resourceGrowthRate=2,
+               nSteps=1000,dimX=30,dimY=30,memory=FALSE,
                decisionType=c("greedy","probabilistic"),
                plot=TRUE,verbose=TRUE)
     {
         population=numeric() #placeholder for recording population size 
-        resource=matrix(round(runif(xDim*yDim,0,maxEnergy)),nrow=xDim,ncol=yDim) #initialise resource scape
+        resource=matrix(round(runif(dimX*dimY,0,maxEnergy)),nrow=dimX,ncol=dimY) #initialise resource scape
         maxResource=resource #maximum possible resource value per cell
         #initialise agents as a data.frame:
-        agents=data.frame(energy=rep(maxEnergy/2,nAgents),x=ceiling(runif(nAgents,0,xDim)),y=ceiling(runif(nAgents,0,xDim)))
+        agents=data.frame(energy=rep(maxEnergy/2,nAgents),x=ceiling(runif(nAgents,0,dimX)),y=ceiling(runif(nAgents,0,dimX)))
 
         #Create cognitive map list in case of memory
            if(memory==TRUE)
                {
                 cognitiveMaps=vector("list",length=nAgents)
-                for (x in 1:nAgents){cognitiveMaps[[x]]=matrix(NA,nrow=xDim,ncol=yDim)}
+                for (x in 1:nAgents){cognitiveMaps[[x]]=matrix(NA,nrow=dimX,ncol=dimY)}
                }
 
         #Start of the actual simulation#
-        if(verbose==TRUE){pb <- txtProgressBar(min = 1, max = timeSteps, style = 3)}
+        if(verbose==TRUE){pb <- txtProgressBar(min = 1, max = nSteps, style = 3)}
 
-        for (t in 1:timeSteps)
+        for (t in 1:nSteps)
             {
 
                 #STEP 1: Observe, Move, and Collect Energy#
@@ -57,16 +54,15 @@ main<-function(nAgents=100,spendEnergy=9,maxEnergy=100,resourceGrowthRate=2,
                                 if (memory==FALSE)
                                     {    
                                         agents[a,2:3]=neighbourhood(xcor=agents[a,2],ycor=agents[a,3],
-                                                  xLimit=c(1,xDim),yLimit=c(1,yDim),
+                                                  xLimit=c(1,dimX),yLimit=c(1,dimY),
                                                   resourceMatrix=resource,type=decisionType)
                                     }
 
                                 if (memory==TRUE)
                                     {
                                         #agents learn and move
-
                                         tmp=getEnvironmentAndMove(xcor=agents[a,2],ycor=agents[a,3],
-                                            xLimit=c(1,xDim),yLimit=c(1,yDim),
+                                            xLimit=c(1,dimX),yLimit=c(1,dimY),
                                             resourceMatrix=resource,type=decisionType,myMap=cognitiveMaps[[a]])
                                         
                                         cognitiveMaps[[a]]=tmp[[1]]
@@ -94,7 +90,7 @@ main<-function(nAgents=100,spendEnergy=9,maxEnergy=100,resourceGrowthRate=2,
                     }
 
                 #STEP 3: Spend Energy#
-                agents$energy=agents$energy-spendEnergy
+                agents$energy=agents$energy-energyCost
 
 
                 #STEP 4: Death#
@@ -128,7 +124,7 @@ main<-function(nAgents=100,spendEnergy=9,maxEnergy=100,resourceGrowthRate=2,
                         par(mfrow=c(1,2))
                         plot(1:t,population[1:t],type="l",xlab="time",ylab="population",
                              main=paste("Avg.Energy=",round(mean(agents$energy),2)))
-                        image(x=1:xDim,y=1:yDim,z=resource,main=paste("Avg.Resource=",round(mean(resource),2)),zlim=c(0,maxEnergy))
+                        image(x=1:dimX,y=1:dimY,z=resource,main=paste("Avg.Resource=",round(mean(resource),2)),zlim=c(0,maxEnergy))
                         points(agents$x,agents$y,pch=20,cex=2)
                     }
 
@@ -150,27 +146,36 @@ step=c(-1,0,1)
 xcor1=xcor+step
 ycor1=ycor+step
 address=expand.grid(x=xcor1,y=ycor1)
+noMove=address[5,]
+
 if(sum((address$x<xLimit[1]|address$x>xLimit[2]),na.rm=TRUE)>0){address[which(address$x<xLimit[1]|address$x>xLimit[2]),]=NA}
 if(sum((address$y<yLimit[1]|address$y>yLimit[2]),na.rm=TRUE)>0){address[which(address$y<yLimit[1]|address$y>yLimit[2]),]=NA}
+
+
 destinationResource=apply(address,1,function(x,y){
     if(!is.na(x[1]))
         {return(y[x[1],x[2]])}
     if(is.na(x[1]))
         return(NA)},y=resourceMatrix)
-if(type=="greedy")
-    {
-        goto=which(destinationResource==max(destinationResource,na.rm=TRUE))
-        if(length(goto)>1){goto=sample(goto,size=1)}
-        finaladdress=address[goto,]
-    }
-if(type=="probabilistic")
-    {
-        destinationResource[which(is.na(destinationResource))]=0
-        goto=sample(1:9,size=1,prob=destinationResource)
-        if(length(goto)>1){goto=sample(goto,size=1)}
-        finaladdress=address[goto,]
-    }
-return(finaladdress)
+
+if (all(destinationResource==0,na.rm=TRUE)) {return(noMove)}
+else {
+
+    if(type=="greedy")
+        {
+            goto=which(destinationResource==max(destinationResource,na.rm=TRUE))
+            if(length(goto)>1){goto=sample(goto,size=1)}
+            finaladdress=address[goto,]
+        }
+    if(type=="probabilistic")
+        {
+            destinationResource[which(is.na(destinationResource))]=0
+            goto=sample(1:9,size=1,prob=destinationResource)
+            if(length(goto)>1){goto=sample(goto,size=1)}
+            finaladdress=address[goto,]
+        }
+    return(finaladdress)
+      }
 }
 
 
@@ -181,7 +186,7 @@ getEnvironmentAndMove<-function(xcor,ycor,xLimit,yLimit,resourceMatrix,myMap,typ
         xcor1=xcor+step
         ycor1=ycor+step
         address=expand.grid(x=xcor1,y=ycor1)
-        
+        noMove=address[5,]
         if(sum((address$x<xLimit[1]|address$x>xLimit[2]),na.rm=TRUE)>0)
             {address[which(address$x<xLimit[1]|address$x>xLimit[2]),]=NA}
         if(sum((address$y<yLimit[1]|address$y>yLimit[2]),na.rm=TRUE)>0)
@@ -196,10 +201,11 @@ getEnvironmentAndMove<-function(xcor,ycor,xLimit,yLimit,resourceMatrix,myMap,typ
                 if(is.na(past)){myMap[address[x,1],address[x,2]]=current}
                 else if (!is.na(past)){myMap[address[x,1],address[x,2]]=c(past+current)/2}
             }
-
         destinationResource=apply(address,1,function(x,y){return(y[x[1],x[2]])}
            ,y=myMap)
-
+        if (all(destinationResource==0,na.rm=TRUE)){return(list(myMap,noMove))}
+        
+        else {
         if(type=="greedy")
             {
                 goto=which(destinationResource==max(destinationResource,na.rm=TRUE))
@@ -209,10 +215,13 @@ getEnvironmentAndMove<-function(xcor,ycor,xLimit,yLimit,resourceMatrix,myMap,typ
         if(type=="probabilistic")
             {
                 destinationResource[which(is.na(destinationResource))]=0
+                if (all(destinationResource==0)){} #if all destinations are 0
+                
                 goto=sample(1:nrow(address),size=1,prob=destinationResource)
                 if(length(goto)>1){goto=sample(goto,size=1)}
                 finaladdress=address[goto,]
             }
         
         return(list(myMap,finaladdress))
+            }
     }
